02d96c993ecb0e63d0f2c13206b6b9dd1bf9d045
Игорь Рябухин <igorr1970@mail.ru>
2024-10-27 21:44:40 +0500

    Update module_3_1.py

--- module_3_1.py
+++ module_3_1.py
@@ -5,26 +5,24 @@ def count_calls():
     calls += 1
 
 def string_info(string):
-    count_calls()  # Увеличиваем счётчик вызовов
+    count_calls()
     length = len(string)
     upper_case = string.upper()
     lower_case = string.lower()
     return (length, upper_case, lower_case)
 
 def is_contains(string, list_to_search):
-    count_calls()  # Увеличиваем счётчик вызовов
-    # Проверяем наличие строки в списке без учета регистра
+    count_calls()
+    
     for item in list_to_search:
         if item.lower() == string.lower():
             return True
     return False
 
-# Примеры вызовов функций
-print(string_info('Capybara'))  # (8, 'CAPYBARA', 'capybara')
-print(string_info('Armageddon'))  # (10, 'ARMAGEDDON', 'armageddon')
-print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))  # True
-print(is_contains('cycle', ['recycling', 'cyclic']))  # False
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN']))
+print(is_contains('cycle', ['recycling', 'cyclic']))
 
-# Вывод количества вызовов
-print(calls)  # Выводит количество вызовов
+print(calls)  
 
